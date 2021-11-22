0dc40a2e9f13688f35616f4875706b5793f4ca27
C2thehris <chriskreat@hotmail.com>
2021-11-22 10:19:55 -0500

    Hopefully everything works

--- IngredientDispenser/IngredientDispenser.ino
+++ IngredientDispenser/IngredientDispenser.ino
@@ -4,34 +4,34 @@
 const float RAW_STEP_ANGLE = 5.625;
 const float PULL_IN_SPEED = 600;
 const float STEP_ANGLE = RAW_STEP_ANGLE / 25;
-const float STEPPER_RPM = RAW_STEP_ANGLE * PULL_IN_SPEED / 360;
-const int STEPS_PER_REVOLUTION = (360 / STEP_ANGLE);
+const float STEPPER_RPM = 2 * RAW_STEP_ANGLE * PULL_IN_SPEED / 360;
+const int STEPS_PER_REVOLUTION = (180 / STEP_ANGLE);
 
 // Digital IO PINS
 const int CUPS_EIGHTH = 0;
 const int CUPS_FOURTH = 1;
 const int CUPS_HALF = 2;
 const int CUPS_FULL = 3;
-const int CUPS_FULL_PLUS = -1;
+const int CUPS_THIRD = -1;
 const int MODE_SWITCH = 4;
 const int DISPENSE = 5;
 const int TRI_R = 6;
 const int TRI_G = 7;
 const int TRI_B = 8;
 const int SERVO = 9;
-const int STEPPER_THREE = 10;
-const int STEPPER_ONE = 11;
-const int STEPPER_FOUR = 12;
-const int STEPPER_TWO = 13;
+const int STEPPER_WHITE = 10;
+const int STEPPER_ORANGE = 11;
+const int STEPPER_YELLOW = 12;
+const int STEPPER_BLUE = 13;
 
 // Analog IO PINS
 const int OUTPUT_FORCE_SENSOR = A0;
 const int DISPENSING_INDICATOR = A1;
 const int ERROR_PREVENTION_PHOTO = A2;
 
-const int DENSITY = 100;
+const int DENSITY = 185;
 const int WEIGHT_PER_CUP = DENSITY;
-const int VOLUME_PER_ROTATION = -1;
+const float VOLUME_PER_ROTATION = .15;
 
 const int MEASUREMENT_BUTTONS = 5;
 const int PLUS_MULTIPLIER = -1;
@@ -40,11 +40,11 @@ bool dispense_state = false;
 bool err = true;
 bool selected_amount[MEASUREMENT_BUTTONS];
 
+unsigned long last_stir = 0;
 
-int last_stir = 0;
 // initialize the stepper library on pins 8 through 11:
-Stepper auger_stepper(STEPS_PER_REVOLUTION, STEPPER_ONE,
-                      STEPPER_TWO, STEPPER_THREE, STEPPER_FOUR);
+Stepper auger_stepper(STEPS_PER_REVOLUTION, STEPPER_WHITE,
+                      STEPPER_YELLOW, STEPPER_ORANGE, STEPPER_BLUE);
 Servo paddle_servo;
 
 void set_LED(bool r_state, bool g_state, bool b_state)
@@ -55,14 +55,25 @@ void set_LED(bool r_state, bool g_state, bool b_state)
 }
 
 void setup() {
-  // nothing to do inside the setup
   Serial.begin(9600);
   paddle_servo.attach(SERVO);
   auger_stepper.setSpeed(STEPPER_RPM);
+  pinMode(TRI_R, OUTPUT);
+  pinMode(TRI_G, OUTPUT);
+  pinMode(TRI_B, OUTPUT);
+
+  pinMode(CUPS_EIGHTH, INPUT);
+  pinMode(CUPS_FOURTH, INPUT);
+  pinMode(CUPS_HALF, INPUT);
+  pinMode(CUPS_FULL, INPUT);
+  pinMode(CUPS_THIRD, INPUT);
+  pinMode(MODE_SWITCH, INPUT);
+  pinMode(DISPENSE, INPUT);
+  
   set_LED(HIGH, HIGH, HIGH);
 }
 
-bool value_set(bool* selected_amount)
+bool value_set()
 {
   for (int i = 0; i < MEASUREMENT_BUTTONS; ++i)
   {
@@ -74,7 +85,7 @@ bool value_set(bool* selected_amount)
 
 void drive_stepper(float revolutions, Stepper& stepper)
 {
-  stepper.step(2 * revolutions * STEPS_PER_REVOLUTION);
+  stepper.step(revolutions * STEPS_PER_REVOLUTION);
 }
 
 void set_servo_pos(Servo &servo, int pos)
@@ -106,9 +117,10 @@ float get_volume(bool* selected_amount, bool by_weight)
   return volume;
 }
 
-void stir(const Servo& servo)
+void stir(Servo& servo)
 {
-  
+  servo.write(180);
+  servo.write(0);
 }
 
 void dispense(float volume)
@@ -116,27 +128,36 @@ void dispense(float volume)
   drive_stepper(volume / VOLUME_PER_ROTATION, auger_stepper);
 }
 
-void LED_state(int state)
-  //0 = error not triggered
-  //1 = error triggered, no button pressed
-  //2 = button pressed, ready to dispense
-  //3 = dispensing
+void read_buttons()
 {
-  if (state == 0)
+  selected_amount[0] = digitalRead(CUPS_EIGHTH);
+  selected_amount[1] = digitalRead(CUPS_FOURTH);
+  selected_amount[2] = digitalRead(CUPS_THIRD);
+  selected_amount[3] = digitalRead(CUPS_HALF);
+  selected_amount[4] = digitalRead(CUPS_FULL);
+}
+
+
+void LED_state(int state)
+  // -1/NOTB = Error
+  //       0 = error not triggered
+  //       2 = input + no error: ready to dispense
+  //       3 = dispensing
+{
+  switch (state)
   {
-    set_LED(LOW, LOW, LOW);
-  }
-  else if (state == 1)
-  {
-    set_LED(LOW, LOW, HIGH);
-  }
-  else if (state == 2)
-  {
-    set_LED(LOW, HIGH, LOW);
-  }
-  else if (state == 3)
-  {
-    set_LED(LOW, HIGH, HIGH);
+    case 0:
+      set_LED(LOW, LOW, LOW);
+      break;
+    case 2:
+      set_LED(LOW, HIGH, LOW);
+      break;
+    case 3:
+      set_LED(LOW, HIGH, HIGH);
+      break;
+    default:
+      set_LED(HIGH, LOW, LOW);
+      break;
   }
 }    
 
@@ -154,33 +175,26 @@ void loop() {
    LED_state(0);
   }
   
-  selected_amount[0] = digitalRead(CUPS_EIGHTH);
-  selected_amount[1] = digitalRead(CUPS_FOURTH);
-  selected_amount[2] = digitalRead(CUPS_HALF);
-  selected_amount[3] = digitalRead(CUPS_FULL);
-  selected_amount[4] = digitalRead(CUPS_FULL_PLUS);
+  read_buttons();
   bool mode = digitalRead(MODE_SWITCH);
   if (value_set(selected_amount))
   {
     LED_state(2);
   }
 
-  bool enable_dispenser = false;
-  if (dispense_state = !digitalRead(DISPENSE))
+  if (dispense_state == !digitalRead(DISPENSE))
   {
-    enable_dispenser = true;
+    dispense_state = !dispense_state;
     LED_state(3);
-  }
-  dispense_state = digitalRead(DISPENSE);
-  
-  if (enable_dispenser && value_set(selected_amount))
-  {
-    int volume = get_volume(selected_amount, mode);
-    dispense(volume);
+    if (value_set())
+    {
+      int volume = get_volume(selected_amount, mode);
+      dispense(volume); 
+    }
   }
   
   unsigned long current_time = millis();
-  if (current_time - last_stir > 15000)
+  if (current_time - last_stir > 15000 || current_time < last_stir)
   {
     last_stir = current_time;
     stir(paddle_servo);
